22fbd4a8e96afd15e454e086dadd85cdd1a981fc
Hennie Scheepers <hennie.104@hotmail.com>
2023-04-04 12:50:51 +0200

    Added missing space to line 7

--- IWA-6.1/script.js
+++ IWA-6.1/script.js
@@ -4,7 +4,7 @@ const secondaryPhone = '0219231568'
 // Only chnage below this line
 
 // used isNaN to determine if primaryPhone and secondaryPHone would return NaN when converted to number
-const primaryValid = isNaN(primaryPhone) === true ?false : true
+const primaryValid = isNaN(primaryPhone) === true ? false : true
 const secondaryValid = isNaN(secondaryPhone) === true? false : true
 
 console.log('Priamry phone is valid numerical string:', primaryValid)
