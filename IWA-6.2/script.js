45981061543d067c7a5a42c49c96a5ab2b66da1d
Hennie Scheepers <hennie.104@hotmail.com>
2023-04-04 13:47:12 +0200

    Completed challenge 2 of IWA-6

---
+++ IWA-6.2/script.js
@@ -0,0 +1,24 @@
+const rent = 400;
+//changed tax value to represent percentage
+const tax = 12;
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
+
+// Only change below this line
+
+//Removed quotation marks around numbers
+const taxAsDecimal = tax / 100
+const startingAfterTax = salary * (1 - taxAsDecimal)
+//fixed spelling error in balance and startingAfterTax
+const balance = startingAfterTax - transport - food - rent
+//Added missing curly bracket before hourOfDay
+
+console.log(hourOfDay, minuteOfDay)
+// Gave hourOfDay and minuteOfDay their own undefined check. Proogram doesn't work with hourOfDay && minuteOfday !== undefined
+if ((hourOfDay === 00 && minuteOfDay === 00) && (hourOfDay !== undefined && minuteOfDay !== undefined)) {
+    console.log(balance.toFixed(2))
+}
+
